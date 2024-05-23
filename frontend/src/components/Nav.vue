23b8e43042da349b62fe0476178f2ccfc453f90e
Kacper Wilk <tyger049@gmail.com>
2024-05-23 17:15:28 +0200

    prod build 202405231715

--- frontend/src/components/Nav.vue
+++
@@ -1,9 +0,0 @@
-<template>
-      <v-navigation-drawer :width="255" color="#171a21">
-          <v-list-item base-color="#1a9fff"><div class="navtitle"><span id="logonav">CWL</span><v-icon class="ml-1 d-flex" size="30" icon="mdi-pistol"></v-icon></div></v-list-item>
-          <v-divider></v-divider>
-          <v-divider></v-divider>
-          <v-list-item base-color="#88bde9" link>Work in progress<v-icon class="ml-2" icon="mdi mdi-account-hard-hat-outline"></v-icon></v-list-item>
-          <v-divider></v-divider>
-      </v-navigation-drawer>
-</template>
