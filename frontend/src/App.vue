6c442b714f6032b428d09076c64d17b55bf54801
Kacper Wilk <tyger049@gmail.com>
2024-05-24 21:04:04 +0200

    added google analytics

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,15 @@ import Navbar from '@/components/Nav.vue'
 
 
 </script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-E9QY5M2G75"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-E9QY5M2G75');
+</script>
 
 <template>
   <header>
