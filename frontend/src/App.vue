3334015953b877716e780f2ad16cebbc2fa2fceb
Kacper Wilk <tyger049@gmail.com>
2024-05-24 21:07:56 +0200

    vue gtag

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,8 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from '@/components/Nav.vue'
-
-
 </script>
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-E9QY5M2G75"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-E9QY5M2G75');
-</script>
 
 <template>
   <header>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -10,6 +10,7 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import '@mdi/font/css/materialdesignicons.css'
+import VueGtag from "vue-gtag";
 
 
 const vuetify = createVuetify({
@@ -21,4 +22,8 @@ const app = createApp(App)
 
 app.use(router)
 app.use(vuetify)
+app.use(VueGtag, {
+  config: { id: "G-E9QY5M2G75" }
+})
 app.mount('#app')
+
